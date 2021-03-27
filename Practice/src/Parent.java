f281465c11a08600d9875d102fdae174e728981c
brtrduon <brianduongt@gmail.com>
2021-03-27 15:20:56 -0700

    added java practice

---
+++ Practice/src/Parent.java
@@ -0,0 +1,17 @@
+
+public class Parent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// this step prints in the console
+		System.out.println("Navigated to home page");
+		
+		Methods m = new Methods();
+
+		m.ValidateHeader();
+		
+		// Classoject.methodname
+	}
+	
+}
