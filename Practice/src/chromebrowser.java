e6db44b22efce38116d87518ec2f0cf808d1f053
brtrduon <brianduongt@gmail.com>
2021-04-04 16:53:01 -0700

    added xpath and css identifyers

--- Practice/src/chromebrowser.java
+++ Practice/src/chromebrowser.java
@@ -17,12 +17,16 @@ public class chromebrowser{
 //		driver.findElement(By.name("pass")).sendKeys("123456");
 //		driver.findElement(By.linkText("Forgot Password?")).click();
 		
-		driver.findElement(By.className("inputtext")).sendKeys("Hello World");
+		driver.findElement(By.cssSelector("#email")).sendKeys("emailaddress");
+		driver.findElement(By.xpath("//*[@id=\"pass\"]")).sendKeys("password");
+		driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div[1]/form/div[2]/button")).click();
+		
+//		driver.findElement(By.className("inputtext")).sendKeys("Hello World");
 		
 //		driver.get("https://login.salesforce.com/");
 //		driver.findElement(By.id("username")).sendKeys("Hello");
 //		driver.findElement(By.name("pw")).sendKeys("123456");
 //		driver.findElement(By.className("button r4 wide primary")).click();
-		
+//		driver.findElement(By.xpath("//*[@id=\'Login\']")).click();
 	}
 }
