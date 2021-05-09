fc4a32e5a191af3fd6874927b0f8ec4300afc8d6
brtrduon <brianduongt@gmail.com>
2021-05-09 12:52:38 -0700

    added ecommerce add to cart

--- Practice/src/Assignment.java
+++ Practice/src/Assignment.java
@@ -10,15 +10,15 @@ public class Assignment {
 		System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
-		
+
 		driver.findElement(By.id("name")).sendKeys(text);
 		driver.findElement(By.cssSelector("[id='alertbtn']")).click();
 		System.out.println(driver.switchTo().alert().getText());
 		driver.switchTo().alert().accept();
-		
+
 		driver.findElement(By.id("confirmbtn")).click();
 		System.out.println(driver.switchTo().alert().getText());
 		driver.switchTo().alert().dismiss();
 	}
-
+	
 }
